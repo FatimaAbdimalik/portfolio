933073eb49f4858b67a847a8ef150944eff5e123
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-10-11 22:56:48 +0100

    improved sidebar background colour

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { CSSTransition } from "react-transition-group";
 import Home from "./components/Home/Home.js";
 import About from "./components/About/About.js";
 import Skills from "./components/Skills/Skills.js";
 import Projects from "./components/Projects/Projects.js";
 import Contact from "./components/Contact/Contact.js";
-import Footer from "./components/Footer/Footer.js";
 import "./App.css";
 import "./index.css";
 function App() {
   return (
     <div className="App">
+      {/* <CSSTransition in={loading} timeout={400} classNames="fade" unmountOnExit>
+  <Loader />
+ </CSSTransition> */}
       <Router>
         <div className="content">
           <Switch>
