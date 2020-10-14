74f04c70795642a8f6bd90994cc39001375f6579
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-10-14 16:57:18 +0100

    modified some components

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SideBar from "../SideBar/SideBar";
-import Footer from "../Footer/Footer.js";
+import { Form } from "react-bootstrap";
 
 const Contact = () => {
   return (
@@ -20,13 +20,39 @@ const Contact = () => {
               name="name"
               className="input_field"
             />
-            <input placeholder="Email" type="text" name="email" />
+            <input
+              placeholder="Email"
+              type="text"
+              name="email"
+              className="input_field"
+            />
+            <br />
+            <input
+              placeholder="Subject"
+              type="text"
+              name="subject"
+              className="subject-field"
+            />
             <br />
             <textarea name="message" rows="4" cols="50" />
           </div>
           <div>
             <button>SEND</button>
           </div>
+          {/* <Form>
+            <Form.Group controlId="name">
+              <Form.Control type="name" placeholder="name" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group controlId="subject">
+              <Form.Control type="subject" placeholder="subject" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Control as="textarea" rows="3" col="5" />
+            </Form.Group>
+          </Form> */}
         </div>
       </div>
     </div>
