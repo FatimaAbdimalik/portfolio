90822bc9b0d9ec0258541c672479f451ee83729e
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-10-17 09:47:49 +0100

    Updates

---
+++ precache-manifest.38837320f2f392dc29208f5ff52a5e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20cef30462cb96ae8c6a61fe7258aafd",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "2f25a1da3b4f3bc73869",
+    "url": "/react-portfolio/static/css/main.88f54964.chunk.css"
+  },
+  {
+    "revision": "70b428f1516e76d22e48",
+    "url": "/react-portfolio/static/js/2.6db76d78.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.6db76d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f25a1da3b4f3bc73869",
+    "url": "/react-portfolio/static/js/main.bf923e52.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
